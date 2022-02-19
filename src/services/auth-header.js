8701dfe66d7a33a7d9cd5613fbb87a9cc622b454
WesleyWong <31143878+Wesley16838@users.noreply.github.com>
2022-02-19 17:35:45 -0600

    change token template

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,9 +1,9 @@
 export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.accessToken) {
-      return { Authorization: 'Bearer ' + user.accessToken };
+      // return { Authorization: 'Bearer ' + user.accessToken };
     // Use for Node.js Express API
-    // return { 'x-access-token': user.accessToken };
+      return { 'x-access-token': user.accessToken };
     } else {
       return {};
     }
